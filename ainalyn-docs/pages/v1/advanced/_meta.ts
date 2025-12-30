8ee351c3bbe6035f5406bef7129283c013854e83
Joel Hsu(徐捷耀) <joelhsu@febigcity.com>
2025-12-30 13:48:53 +0800

    chore: add diagram to document and modify architecture content

--- ainalyn-docs/pages/v1/advanced/_meta.ts
+++ ainalyn-docs/pages/v1/advanced/_meta.ts
@@ -1,3 +1,5 @@
 export default {
-  "architecture-overview": "Architecture Overview (Contributors)"
+  "architecture-overview": "Architecture & Code Guide",
+  "system-context": "System Context",
+  "compilation-flow": "Compilation Flow"
 }
